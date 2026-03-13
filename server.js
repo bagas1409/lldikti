74273c6a624181f645a89e5b3e7a51a80654ae4e
bagas1409 <bagaspermana1409@gmail.com>
2026-03-13 22:16:33 +0700

    update db init and server setup2

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 import app from "./src/app.js";
-import { initDatabase } from "./src/config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-const startServer = async () => {
-  await initDatabase();
-
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-};
-
-startServer();
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
